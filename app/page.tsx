e99a1bc89e99a95e3db23c2ba2c6f77507518735
buttje-web <webmaster@buttje.at>
2025-08-31 20:47:10 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -89,7 +89,6 @@ export default function Home() {
   }, []);
 
   return (
-
       {/* Hero Section */}
       <section className="relative min-h-[calc(100vh-4rem)] flex items-start md:items-center justify-center pt-32 pb-32 md:py-0" aria-label="Willkommen">
         {/* Hero Background Video */}
