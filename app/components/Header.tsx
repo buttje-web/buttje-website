33967bb9142bd9045e72d23a96b24e4535ce16c6
buttje-web <webmaster@buttje.at>
2025-10-06 16:14:38 +0200

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -22,8 +22,13 @@ export default function Header() {
               </div>
             </div>
           </Link>
-
-          {/* Desktop Navigation */}
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NV4H5V3C');</script>
+<!-- End Google Tag Manager -->          {/* Desktop Navigation */}
           <div className="hidden md:flex gap-8">
             <Link href="/#dienstleistungen" className="text-gray-900 hover:text-[var(--primary)]">
               Dienstleistungen
